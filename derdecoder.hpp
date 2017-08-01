bec0b1d02c3b2303777c95902327443bc57a2eff
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2017-07-31 22:02:01 -0400

    skeleton for primitive value parsing
    Go by class, then type. Now parses end of input and Boolean values.

--- derdecoder.hpp
+++ derdecoder.hpp
@@ -10,7 +10,7 @@ public :
 	DERDecoder& operator=DERDecoder(DERDecoder &&) = default;
 
 	template < typename InputIterator >
-	bool. parse(InputIterator &first, InputIterator last)
+	bool parse(InputIterator &first, InputIterator last)
 	{
 		while (first != last)
 		{
@@ -26,10 +26,14 @@ public :
 				state_ = State::expect_value__;
 				break;
 			case State::expect_value__ :
-				
+				bool complete(parseValue(first, last));
+				if (!complete) return false;
+				state_ = State::expect_type__;
 			}
 		}
 	}
+	
+	virtual void onEndOfInput() noexcept = 0;
 	
 private :
 	enum struct State {
@@ -48,7 +52,7 @@ private :
 	{
 		TypeClass type_class_;
 		bool constructed_;
-		uint8_t tag_number_;
+		uint8_t tag_;
 	};
 	
 	void parseType(unsigned char tag)
@@ -69,8 +73,8 @@ private :
 			break;
 		}
 		type_.constructed_ = ((tag & 0x20) == 0x20);
-		type_.tag_number_ = tag & 0x1F;
-		if (0x1F == type_.tag_number_)
+		type_.tag_ = tag & 0x1F;
+		if (0x1F == type_.tag_)
 		{
 			throw NotYetImplemented("multi-byte type tags are not yet implemented");
 		}
@@ -83,9 +87,9 @@ private :
 	{
 		if (first == last) return false;
 		assert(length_buffer_size_ < sizeof(length_buffer_));
-		// When we get here, we know we at least one extra byte to parse to get
-		// the length. We also know that whatever other bytes make up the
-		// length are either waiting in the input or already in the length
+		// When we get here, we know we have at least one extra byte to parse
+		// to get the length. We also know that whatever other bytes make up
+		// the length are either waiting in the input or already in the length
 		// buffer, so we add one byte the input into the length buffer and
 		// try to parse the length buffer as a length. If the of the length
 		// is greater than 64 bits, we bail out.
@@ -130,6 +134,60 @@ private :
 		} while(first != last);
 		
 		return false;
+	}
+	
+	template < typename InputIterator >
+	bool parseValue(InputIterator &first, InputIterator last)
+	{
+		switch (type_.type_class_)
+		{
+		case TypeClass::universal__ : return parseUniversalValue(first, last);
+		case TypeClass::application__ : return parseApplicationValue(first, last);
+		case TypeClass::context_specific__ : return parseContextSpecificValue(first, last);
+		case TypeClass::private__ : return parsePrivateValue(first, last);
+		}
+		return false;
+	}
+	
+	template < typename InputIterator >
+	bool parseUniversalValue(InputIterator &first, InputIterator last)
+	{
+		assert(type_.type_class_ == TypeClass::universal__);
+		if (type_.constructed_)
+		{
+		}
+		else 
+		{
+			switch (type_.tag_)
+			{
+			case 0x00 : // end of input
+				if (0 != length_) throw EncodingError("Non-zero length for end-of-input tag");
+				onEndOfInput();
+				return true;
+			case 0x01 : // boolean
+				if (length_ != 1) throw EncodingError("Wrong length for Boolean value");
+				if (first == last) return false; // no input, but an error 
+				unsigned char value(*first++);
+				onBoolean(0 != value);
+				return true;
+			// HERE
+			}
+		}
+	}
+	
+	template < typename InputIterator >
+	bool parseApplicationValue(InputIterator &first, InputIterator last)
+	{
+	}
+	
+	template < typename InputIterator >
+	bool parseContextSpecificValue(InputIterator &first, InputIterator last)
+	{
+	}
+	
+	template < typename InputIterator >
+	bool parsePrivateValue(InputIterator &first, InputIterator last)
+	{
 	}
 	
 	State state_ = initial__;
