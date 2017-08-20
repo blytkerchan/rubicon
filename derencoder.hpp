fbcb1df40d4358f01b162270abc3bff57ba6856d
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2017-08-20 19:25:59 -0400

    encode reals
    Real encoding is among the more complex encodings in DER. It doesn't
    follow IEEE-754 at all, so we have to extract the mantissa and the
    exponent, in the case of the mantissa decide whether to represent it
    in base 2, 8 or 16 and in either case encode the sign and the value.
    Subnormal values resolve to zero in the mantissa and can't
    represented, so they will hit am assertion on line 175 of this code.
    
    I should note that I heartily dislike floating points for various
    reasons, not least of which quirks like subnormal values, epsilon,
    whacky encodings, etc. Add to that that most, if not all, floating
    point values "in the wild" are really either a function of some
    irrational number (pi, e, ...), a fraction, or an integer measurement
    with scaling (gain and offset) lossy representations like IEEE-754 or
    this one are really unnecessary (just store the function, fraction, or
    measurements in stead).

--- derencoder.hpp
+++ derencoder.hpp
@@ -62,6 +62,120 @@ public :
 		out = std::copy(first, last, out);
 	}
 	
+	template < typename OutputIterator >
+	void encodeReal(OutputIterator &out, double value)
+	{
+		*out++ = 0x09;
+		// a few special values to handle first 
+		switch (std::fpclassify(value))
+		{
+		case FP_INFINITE	:
+			encodeLength(out, 1);
+			*out++ = std::signbit(value) ? 0x41 : 0x40;
+			return;
+		case FP_NAN			:
+			encodeLength(out, 1);
+			*out++ = 0x42;
+			return;
+		case FP_ZERO		:
+			if (std::signbit(value))
+			{
+				encodeLength(out, 1);
+				*out++ = 0x43;
+			}
+			else 
+			{
+				encodeLength(out, 0);
+			}
+			return;
+		default :
+			// either normal or subnormal, we'll encode 
+			break;
+		}
+		
+		int exponent;
+		double mantissa(frexp(value, &exponent));
+		Details::Integer exponent_as_integer(exponent);
+		// the first encoded value octet doesn't have a name in X.690, but it
+		// *indicates* the way the value is encoded, so I decided to call it
+		// the "indicator octet".
+		unsigned char indicator_octet(0x80); // bit 8(7) to indicate binary encoding 
+		indicator_octet |= std::signbit(value) ? 0x40 : 0x00;
+		uint64_t mantissa_as_int(mantissa);
+		unsigned int base(2);
+		if ((mantissa_as_int % 4) == 0)
+		{
+			base *= 4;
+			mantissa_as_int /= 4;
+		}
+		else 
+		{ /* base 8 */ }
+		if ((mantissa_as_int % 2) == 0)
+		{
+			assert(base == 8);
+			base *= 2;
+			mantissa_as_int /= 2;
+		}
+		else 
+		{ /* base is final */ }
+		switch (base)
+		{
+		case 2 :
+			// indicator octet is OK for now 
+			break;
+		case 8 :
+			indicator_octet |= 0x10;
+			break;
+		case 16 :
+			indicator_octet |= 0x20;
+			break;
+		default :
+			throw std::logic_error("unreachable code");
+		}
+		exponent_as_integer.compact();
+		assert(exponent_as_integer.size() <= 255);
+		switch (exponent_as_integer.size())
+		{
+		case 0 :
+			// fall through: if the exponent is zero, were still need an octet
+			// to encode it
+		case 1 :
+			// indicator octet already OK 
+			break;
+		case 2 :
+			indicator_octet |= 0x01;
+			break;
+		case 3 :
+			indicator_octet |= 0x02;
+			break;
+		default :
+			indicator_octet |= 0x03;
+		}
+		mantissa_as_integer.compact();
+		unsigned int const bytes_for_indicator(1);
+		unsigned int const bytes_for_exponent_length(exponent_as_integer.size() > 3 ? 1 : 0);
+		unsigned int const bytes_for_exponent_value(exponent_as_integer.size());
+		unsigned int const bytes_for_mantissa_value(mantissa_as_integer.size());
+		encodeLength(out, bytes_for_indicator + bytes_for_exponent_length + bytes_for_exponent_value + bytes_for_mantissa_value);
+		*out++ = indicator_octet;
+		if (exponent_as_integer.size() > 3)
+		{
+			*out++ = exponent_as_integer.size();
+		}
+		else 
+		{ /* already encoded */ }
+		if (exponent_as_integer.size() == 0)
+		{
+			*out++ = 0;
+		}
+		else 
+		{
+			out = std::copy(exponent_as_integer.begin(), exponent_as_integer.end(), out);
+		}
+		assert(mantissa_as_integer.size() != 0):
+		out = std::copy(mantissa_as_integer.begin(), mantissa_as_integer.end(), out);
+	}
+	
 	template < typename OutputIterator, typename MultiPassInputIterator >
 	void encodeOctetString(
 		  OutputIterator &out 
