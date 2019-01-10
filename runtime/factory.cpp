424186a62bc479c5baf1d90db1ea40e2eacf009a
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2019-01-10 10:47:00 -0500

    add missing file

---
+++ runtime/factory.cpp
@@ -0,0 +1,53 @@
+#include "factory.hpp"
+
+namespace Vlinder { namespace Rubicon { namespace Details {
+/*virtual */void Builder::onEndOfContents()/* override*/
+{
+
+}
+/*virtual */void Builder::onInteger(Integer const &value)/* override*/
+{
+
+}
+/*virtual */void Builder::onEnumerated(int value)/* override*/
+{
+
+}
+/*virtual */void Builder::onBitString(bool final, unsigned int unused_bits, unsigned char *first, unsigned char *last)/* override*/
+{
+
+}
+/*virtual */void Builder::onOctetString(bool final, unsigned char *first, unsigned char *last)/* override*/
+{
+
+}
+/*virtual */void Builder::onNull()/* override*/
+{
+
+}
+/*virtual */void Builder::onBeginSequence()/* override*/
+{
+
+}
+/*virtual */void Builder::onEndSequence()/* override*/
+{
+
+}
+/*virtual */void Builder::onBeginSet()/* override*/
+{
+
+}
+/*virtual */void Builder::onEndSet()/* override*/
+{
+
+}
+/*virtual */void Builder::onBoolean(bool val)/* override*/
+{
+
+}
+/*virtual */void Builder::onReal(double val)/* override*/
+{
+        
+}
+}}}
+
